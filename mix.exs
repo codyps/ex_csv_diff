14d2604e711a502eed721277f6e6085bc0c45187
Cody P Schafer <dev@codyps.com>
2025-11-13 11:01:32 -0600

    wip

--- mix.exs
+++ mix.exs
@@ -6,11 +6,23 @@ defmodule CsvDiff.MixProject do
       app: :csv_diff,
       version: "0.1.0",
       elixir: "~> 1.18",
-      licenses: ["Apache-2.0"],
       source_url: "https://github.com/codyps/ex_csv_diff",
       homepage_url: "https://github.com/codyps/ex_csv_diff",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
+    ]
+  end
+
+  defp description() do
+    "A fast CSV diffing library powered by the Rust csv-diff crate via Rustler."
+  end
+
+  defp package() do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/codyps/ex_csv_diff"}
     ]
   end
 
